f5a3eb3439481001f190229b7f50c226037a44c4
Marcin Makarewicz <marcin@makarewicz.eu>
2024-10-12 22:20:16 +0200

    fix(stl): caps inner diameter

--- packages/stl/src/containers/index.tsx
+++ packages/stl/src/containers/index.tsx
@@ -220,18 +220,17 @@ export const Lathes = function ({ wireframe, meshRef }) {
 };
 
 export const Caps = function ({ wireframe, meshRef }) {
-  const segments = 24;
+  const segments = 36 * 2;
   const points = useMemo(() => {
     const r1 = 15 / 2;
-    const r2 = 13 / 2;
+    const r2 = 13.5 / 2;
     let n = 0;
     return [
       new Vector2(0, n),
-      new Vector2(r1, n++),
+      new Vector2(r1 - 0.2, n),
+      new Vector2(r1, ++n - 0.8),
       new Vector2(r1, ++n),
-      new Vector2(r2, n),
-      new Vector2(r2, ++n),
-      new Vector2(r2 - 1, ++n),
+      new Vector2(r2 - 0.1, n),
       new Vector2(r2, ++n),
       new Vector2(r2 - 1, ++n),
       new Vector2(r2, ++n),
