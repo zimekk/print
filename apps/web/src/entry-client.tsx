eea5c926e4b57b0dc70000f38ae857789d209bdc
Marcin Makarewicz <marcin@makarewicz.eu>
2025-10-19 23:02:47 +0200

    feat(web): app server

--- apps/web/src/entry-client.tsx
+++ apps/web/src/entry-client.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./style.css";
-import App from "./App";
+import App from "@dev/web/src/containers/App";
 
 createRoot(document.getElementById("app")!).render(
   <StrictMode>
